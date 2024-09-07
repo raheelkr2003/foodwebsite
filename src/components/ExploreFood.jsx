a6b48b69a83a78ba23bfda640e0d2620051d3a92
unknown <raheelkr2003@gmail.com>
2024-09-07 18:24:50 +0500

    explor section added

---
+++ src/components/ExploreFood.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./ExploreFood.css"
+
+function ExploreFood({ foods }) {
+  return (
+    <div className='explore-foods'>
+      <h1>Explore Foods</h1>
+      <div className='card-container'>
+        {foods.map((food, index) => (
+          <div key={index} className='explore-food-card'>
+            <img src={food.image} alt={food.name} />
+            <p>{food.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ExploreFood;
